dc5ca40fb05fc5f751f84c1c4124389047f0ae66
Gino Mortillero <g.mortillero@arcanys.com>
2021-09-05 16:53:15 +0800

    Multi screen implementation for Translation History List

--- MyApp2/MainActivity.cs
+++ MyApp2/MainActivity.cs
@@ -2,15 +2,20 @@
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 using AndroidX.AppCompat.App;
+using System.Collections.Generic;
 
 namespace MyApp2
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
@@ -20,18 +25,30 @@ namespace MyApp2
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
+            string translatedNumber = string.Empty;
             translateButton.Click += (sender, e) =>
             {
-                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                // Translate user's alphanumeric phone number to numeric
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                    translatedPhoneWord.Text = string.Empty;
+                    translatedPhoneWord.Text = "";
                 }
                 else
                 {
                     translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
                 }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
             };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
